6bae0ef395e4c4ebba37990e2ff38b992d313214
Andrew Jiang <andrewji@usc.edu>
2017-02-09 21:24:19 -0800

    init express server for receiving new subscriptions

---
+++ src/index.js
@@ -0,0 +1,41 @@
+var express     = require('express');
+var app         = express(); // create express app
+var server      = require('http').Server(app);
+var bodyParser  = require('body-parser');
+var request     = require('request');
+
+// configurations
+app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000));
+
+app.post('/subscribe', function (req, res) {
+  var email = req.body.email.toLowerCase();
+  var sendgridKey = process.env.SENDGRID_KEY;
+
+  var sendgridAPIAddContactsOptions = {
+    url: 'https://api.sendgrid.com/v3/contactdb/recipients',
+    method: 'POST',
+    json: true,
+    headers: {
+      'Authorization': 'Bearer ' + sendgridKey
+    },
+    body: [{ email }]
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 201) {
+      // send sendgrid's confirmation
+      res.json(response.body);
+    } else {
+      // send sendgrid's error
+      res.status(response.statusCode).json(error);
+    }
+  }
+
+  request(sendgridAPIAddContactsOptions, callback);
+});
+
+// start the server
+server.listen(app.get('port'), function () {
+  console.log('Running HackSC Server on port ' + app.get('port'));
+});
