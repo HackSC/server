703f76bf10dbdd02ae87839fe18964caeaef079a
root <root@hacksc.localdomain>
2017-08-09 05:13:46 +0000

    committing a small change from src

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ var bodyParser  = require('body-parser');
 var request     = require('request');
 
 // configurations
+app.use(cors());
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/subscribe', cors(), function (req, res) {
+app.post('/subscribe', function (req, res) {
   // allow cross domain
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
